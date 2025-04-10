6f43029a01665052c08454933972822541c49b11
ChzCharbel <charbel_chz@outlook.com>
2025-04-10 17:44:07 -0600

    restauracion archivo inicio routes

--- controllers/inicio.controller.js
+++ controllers/inicio.controller.js
@@ -1,3 +1,46 @@
+const ciclosModel = require('../models/ciclos.model.js');
+
+exports.get_inicio = (request, response, next) => {
+    const idCiclo = request.params.idCiclo || '';
+
+    console.log('CICLO ESCOLAR RUTA: ' + idCiclo);
+    ciclosModel.fetchAll()
+        .then(result => {
+            const ciclosEscolares = result.rows;
+
+            response.render('inicio.ejs', {
+                titulo: 'inicio',
+                privilegios: request.session.privilegios || [],
+                carrera: request.session.carrera || '',
+                ciclosEscolares: ciclosEscolares,
+                cicloActual: idCiclo,
+                username: request.session.username || '',
+                mail: request.session.mail || '',
+                rol: request.session.rol || '',
+            });
+        })
+        .catch(err => {
+            console.error('Error al obtener ciclos escolares:', err);
+            response.status(500).send('Error interno al cargar ciclos escolares');
+        });
+    };
+
+
+exports.post_guardar = (request, response, next) => {
+    const idCicloEscolar = request.params.idCiclo;
+    const { fechaInicio, fechaFin } = request.body;
+
+
+    const ciclo = new ciclosModel(idCicloEscolar, fechaInicio, fechaFin);
+
+  ciclo.save()
+    .then(() => response.redirect('/inicio/' + idCicloEscolar))
+    .catch(err => {
+      console.error('Error al guardar inscripción:', err);
+      response.status(500).send('Error al guardar inscripción');
+    });
+};
+
 exports.get_inscripciones_guardadas = (req, res, next) => {
     const idCiclo = req.params.idCiclo;
 
