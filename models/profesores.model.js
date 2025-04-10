67dbf15396848a5115218d3d3661187de7f5d17b
XimePerezEscalante <A01751827@tec.mx>
2025-04-10 16:14:13 -0600

    Agregar función en modelo de profesores para hacer fetch de su disponibilidad y contar cuántas horas tiene disponibles

--- models/profesores.model.js
+++ models/profesores.model.js
@@ -11,4 +11,46 @@ module.exports = class Profesor {
       [id]
     );
   }
+
+  static contarHoras(dias) {
+    let diasSemana = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes'];
+    const disponibilidad = [];
+    const disponibilidadDia = [[],[],[],[],[]];
+    const disponibilidadSemana = [];
+    console.log(dias.lunes);
+    for (let count = 0; count < 5; count++) {
+      let horas = '';
+      if (diasSemana[count] == 'lunes') {
+        horas = dias.lunes;
+        console.log('LUNES: ');
+      }
+      else if (diasSemana[count] == 'martes') {
+        horas = dias.martes;
+        console.log('MARTES: ');
+      }
+      else if (diasSemana[count] == 'miercoles') {
+        horas = dias.miercoles;
+        console.log('MIERCOLES: ');
+      }
+      else if (diasSemana[count] == 'jueves') {
+        horas = dias.jueves;
+        console.log('JUEVES: ');
+      }
+      else if (diasSemana[count] == 'viernes') {
+        horas = dias.viernes;
+        console.log('VIERNES: ');
+      }
+      for (let hora of horas) {
+          let digitos = hora.slice(0,2).split(':')[0];
+          disponibilidadDia[count].push(digitos);
+          console.log(digitos);
+      }
+      count++;
+    }
+    console.log(disponibilidadDia);
+  }
+
+  static fetchDisponibilidad(idProfe, idCicloE) {
+    return db.query(`SELECT * FROM "Disponible" WHERE "matriculaProfesor" = $1::text AND "idCicloEscolar" = $2::text`, [idProfe, idCicloE]);
+  }
 };
