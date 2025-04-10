a37916036a5126571decd638d4724d2b7a51e71e
XimePerezEscalante <A01751827@tec.mx>
2025-04-10 17:57:50 -0600

    Añadir href a tabs de Oferta Académica

--- controllers/oferta.controller.js
+++ controllers/oferta.controller.js
@@ -22,24 +22,23 @@ exports.get_oferta = (request, response, next) => {
 }
 
 exports.get_agregar = (request, response, next) => {
-    //necesito el id del ciclo escolar y el de la materia en la ruta
     Materia.fetchByDegree(request.session.carrera).then((data) => {
-    const materias = data[0];
-    const planVersiones = data[1];
-    response.render('oferta_academica.ejs',{
-        titulo: 'oferta_academica',
-        privilegios: request.session.privilegios || [],
-        materias: materias,
-        carrera: request.session.carrera || '',
-        ciclosEscolares: request.session.ciclosEscolares || [],
-        cicloActual: request.params.idCiclo || '',
-        username: request.session.username || '',
-        mail: request.session.mail || '',
-        planes: planVersiones || [],
-        planActual: request.params.idPlan || '',
-        rol: request.session.rol || '',
-    })    
-    })
+        const materias = data[0];
+        const planVersiones = data[1];
+        response.render('oferta_agregar.ejs',{
+            titulo: 'oferta_academica',
+            privilegios: request.session.privilegios || [],
+            materias: materias,
+            carrera: request.session.carrera || '',
+            ciclosEscolares: request.session.ciclosEscolares || [],
+            cicloActual: request.params.idCiclo || '',
+            username: request.session.username || '',
+            mail: request.session.mail || '',
+            planes: planVersiones || [],
+            planActual: request.params.idPlan || '',
+            rol: request.session.rol || '',
+        })
+    }) 
     
 }
 
