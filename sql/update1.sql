cda7c43e886f75365a3feafd3865336d553071b9
ChzCharbel <charbel_chz@outlook.com>
2025-04-14 01:29:01 -0600

    [update] sql file

--- sql/update1.sql
+++ sql/update1.sql
@@ -1,5 +1,7 @@
+ALTER TABLE IF EXISTS public."Profesor" DROP COLUMN IF EXISTS estatus;
+
 ALTER TABLE IF EXISTS public."Profesor"
-    ADD COLUMN estatus character varying[];
+    ADD COLUMN estatus character varying;
 
 UPDATE "Profesor"
 SET estatus = 'active';
