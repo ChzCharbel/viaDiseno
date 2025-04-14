87005e69a40c7fc38445a054d1447748bd680346
ChzCharbel <charbel_chz@outlook.com>
2025-04-14 02:26:38 -0600

    [update] sql file

--- sql/update1.sql
+++ sql/update1.sql
@@ -1,2 +1,18 @@
 INSERT INTO public."Enlista" VALUES ('G003', '100007');
 INSERT INTO public."Enlista" VALUES ('G004', '100007');
+
+UPDATE "Grupo"
+SET
+    "lunesInicio" = '12:00',
+    "lunesFin" = '14:00',
+    "miercolesInicio" = '13:00',
+    "miercolesFin" = '14:00'
+WHERE "idGrupo" = 'G003';
+
+UPDATE "Grupo"
+SET
+    "martesInicio" = '12:00',
+    "martesFin" = '14:00',
+    "juevesInicio" = '08:00',
+    "juevesFin" = '10:00'
+WHERE "idGrupo" = 'G004';
