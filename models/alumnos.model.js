955b66bf394d1120bc0291f268ec51d7f56e373d
XimePerezEscalante <A01751827@tec.mx>
2025-04-15 19:42:36 -0600

    [UPDATE] queries en modelo de alumno para que sean más cortas

--- models/alumnos.model.js
+++ models/alumnos.model.js
@@ -2,19 +2,17 @@ const db = require('../util/database');
 
 module.exports = class Alumno {
     static fetchAll() {
-		return db.query(`SELECT u.nombre_usuario, a.matricula, a.carrera, a.regular, a.semestre
-    FROM alumnos a, usuarios u WHERE a.matricula = u.id_ivd;`);
+		return db.query(`SELECT * from usuarios JOIN alumnos using (id_ivd)`);/* u.nombre_usuario, a.matricula, a.carrera, a.regular, a.semestre
+    FROM alumnos a, usuarios u WHERE a.matricula = u.id_ivd;`);*/
 	}
 	static fetchOne(id){
-		return db.query(`SELECT u.nombre_usuario, a.matricula, a.carrera, a.regular, a.semestre
-    FROM alumnos a, usuarios u WHERE a.matricula = u.id_ivd AND a.matricula = $1::text`, [id]);
+		return db.query(`SELECT * from usuarios u JOIN alumnos using(id_ivd) WHERE u.id_ivd = $1::text`, [id]);
 	}
 
 	static find(nombreUsuario) {
         return db.query(
-            `SELECT u.nombre_usuario, a.matricula, a.carrera, a.regular, a.semestre
-			FROM usuarios u, alumnos a
-			WHERE a.matricula = u.id_ivd AND u.nombre_usuario LIKE $1::text`,
+            `SELECT * from usuarios u JOIN alumnos using(id_ivd)
+			WHERE u.nombre_usuario LIKE $1::text`,
             ['%' + nombreUsuario + '%']);
     }
 }
