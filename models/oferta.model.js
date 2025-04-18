b655df275c1664ee84d8c8d2d1dff54c82ca10a3
XimePerezEscalante <A01751827@tec.mx>
2025-04-18 01:07:41 -0600

    [UPDATE] modelo de oferta para agregar variable del id del plan que se usará cuando se guarde la oferta académica

--- models/oferta.model.js
+++ models/oferta.model.js
@@ -2,15 +2,16 @@ const db = require('../util/database');
 const Materias = require('./materias.model');
 
 module.exports = class OfertaAcademica {
-    constructor(mi_id_ciclo, mis_ids_materias) {
+    constructor(mi_id_ciclo, mis_ids_materias, mi_id_plan) {
         this.idCiclo = mi_id_ciclo;
         this.idsMaterias = mis_ids_materias;
+        this.idPlan = mi_id_plan;
     }
 
     save() {
         /* cadena para guardar toda la oferta con una sola 
         consulta */
-        let consulta = `INSERT INTO ofrece VALUES`;
+        let consulta = `INSERT INTO ciclos_escolares_materias VALUES`;
         let i = 2;
         for (let id of stringIds) {
             if (id != '') {
@@ -25,7 +26,7 @@ module.exports = class OfertaAcademica {
         const ciclo = [this.idCiclo];
         const parametros = ciclo.concat(this.idsMaterias);
         console.log(parametros);
-        return db.query(consulta, parametros);
+        //return db.query(consulta, parametros);
     }
 
     static fetchAll(idCicloE, carrera) {
