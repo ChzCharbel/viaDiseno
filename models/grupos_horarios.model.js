9674c91559cf56bf35f1af9c646ce9d63367909b
ChzCharbel <charbel_chz@outlook.com>
2025-04-20 19:13:09 -0600

    nuevo procedimiento y funcion para obtener grupos corregida

--- models/grupos_horarios.model.js
+++ models/grupos_horarios.model.js
@@ -1,18 +1,28 @@
 const db = require("../util/database");
 
-module.exports = class GrupoHorario{
-    static fetchAll() {
-        return db.query(`SELECT * FROM grupos_horarios`);
-      }
+module.exports = class GrupoHorario {
+  static fetchAll() {
+    return db.query(`SELECT * FROM grupos_horarios`);
+  }
 
-      static async guardarHoras(id_salon, id_ciclo_escolar_materia, dia_semana, hora_inicio, hora_fin) {
-        console.log("Salon:" + id_salon, "Ciclo:" +  id_ciclo_escolar_materia, "Dia:" + dia_semana, "HI:" + hora_inicio,  "HF" + hora_fin);
-        const result = await db.query(
-          `SELECT crear_grupo_completo_ivd($1, $2, $3, $4, $5) AS id_grupo`,
-          [id_salon, id_ciclo_escolar_materia, dia_semana, hora_inicio, hora_fin] // El orden debe coincidir con la función SQL
-        );
-        return result.rows[0].id_grupo;
-      }
-      
-
-}
+  static async guardarHoras(
+    id_salon,
+    id_ciclo_escolar_materia,
+    dia_semana,
+    hora_inicio,
+    hora_fin
+  ) {
+    console.log(
+      "Salon:" + id_salon,
+      "Ciclo:" + id_ciclo_escolar_materia,
+      "Dia:" + dia_semana,
+      "HI:" + hora_inicio,
+      "HF" + hora_fin
+    );
+    const result = await db.query(
+      `SELECT crear_grupo_completo($1, $2, $3, $4, $5) AS id_grupo`,
+      [id_salon, id_ciclo_escolar_materia, dia_semana, hora_inicio, hora_fin] // El orden debe coincidir con la función SQL
+    );
+    return result.rows[0].id_grupo;
+  }
+};
--- sql/update3.sql
+++ sql/update3.sql
@@ -31,3 +31,45 @@ BEGIN
     END IF;
 END;
 $$;
+
+CREATE OR REPLACE FUNCTION crear_grupo_completo(
+    id_salon_param INT,
+    id_ciclo_escolar_materia_param INT,
+    dia_semana_param TEXT,
+    hora_inicio_param TIME,
+    hora_fin_param TIME
+)
+RETURNS INT
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    capacidad_salon INT;
+    nuevo_id_grupo INT;
+BEGIN
+    -- 1. Obtener la capacidad del salón
+    SELECT capacidad INTO capacidad_salon
+    FROM salones
+    WHERE id_salon = id_salon_param
+    LIMIT 1;
+
+    IF capacidad_salon IS NULL THEN
+        RAISE EXCEPTION 'El salón no existe';
+    END IF;
+
+    -- 2. Insertar en grupos
+    INSERT INTO grupos (id_salon, cupo_maximo, cupo_disponible)
+    VALUES (id_salon_param, capacidad_salon, capacidad_salon)
+    RETURNING id_grupo INTO nuevo_id_grupo;
+
+    -- 3. Insertar en grupos_ciclos_materias
+    INSERT INTO grupos_ciclos_materias (id_grupo, id_ciclo_escolar_materia)
+    VALUES (nuevo_id_grupo, id_ciclo_escolar_materia_param);
+
+    -- 4. Insertar en grupos_horarios
+    INSERT INTO grupos_horarios (id_grupo, dia_semana, hora_inicio, hora_fin)
+    VALUES (nuevo_id_grupo, dia_semana_param, hora_inicio_param, hora_fin_param);
+
+    -- Regresar el id_grupo creado
+    RETURN nuevo_id_grupo;
+END;
+$$;
