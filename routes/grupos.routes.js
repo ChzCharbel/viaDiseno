8ab54f0ea0c2f2b6816cd2592598138da1159304
ChzCharbel <charbel_chz@outlook.com>
2025-04-21 18:39:17 -0600

    Archivos para generar horarios de manera automatica

--- routes/grupos.routes.js
+++ routes/grupos.routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const gruposController = require('../controllers/grupos.controller');
+const horariosController = require('../controllers/horarios.controller');
 
 router.post('/asignar-salon', gruposController.asignarSalonPorMateria);
 
 // nueva ruta para fetch("/grupos/datos")
 router.get('/datos', gruposController.getDatosGrupos);
+router.post('/horarios', horariosController.generarHorariosParaTodos);
 
 module.exports = router;
