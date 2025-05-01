2710df3aad589c951de596938d66aa3cfc450e6b
XimePerezEscalante <A01751827@tec.mx>
2025-05-01 00:28:39 -0600

    [UPDATE] ruta para post de edición de semestres

--- routes/oferta.routes.js
+++ routes/oferta.routes.js
@@ -7,12 +7,15 @@ const canRegister = require('../util/canRegisterOfertaAcademica');
 
 const ofertaController = require('../controllers/oferta.controller');
 
+router.get('/:idCiclo/:idPlan/eliminar/:idPlanMateria', isAuth, canRegister, ofertaController.delete_materia);
+
+router.get('/:idCiclo/:idPlan/editar', isAuth, canRegister, ofertaController.get_editar);
+router.post('/:idCiclo/:idPlan/editar', isAuth, canRegister, ofertaController.post_editar);
 
 router.get('/:idCiclo/:idPlan/agregar', isAuth, canRegister, ofertaController.get_agregar);
-router.get('/:idCiclo/:idPlan', isAuth, canView, ofertaController.get_oferta);
-
 router.post('/:idCiclo/:idPlan/agregar', isAuth, canRegister, ofertaController.post_agregar);
 
-router.get('/:idCiclo/:idPlan/eliminar/:idPlanMateria', isAuth, canRegister, ofertaController.delete_materia);
+router.get('/:idCiclo/:idPlan', isAuth, canView, ofertaController.get_oferta);
+
 
 module.exports = router;
