0425aadaba95176819fc6994f5decc4a5d5d8fad
XimePerezEscalante <A01751827@tec.mx>
2025-05-01 22:51:11 -0600

    [UPDATE] cambiar innerText de celda de la tabla donde se comparan las materias asignadas vs las de la oferta académica

--- public/js/calculosGruposAutomaticos.js
+++ public/js/calculosGruposAutomaticos.js
@@ -3,7 +3,11 @@ let horasTotalClase = 0;
 
 // Contar horas totales del grupo a la semana
 function stringToTime(horaInicio, horaFin, idCicloMateria) {
+    // celda de la tabla de los grupos
     const celda = document.getElementById("celda" + idCicloMateria);
+    /* celda de la tabla donde se comparan los grupos con las materias de la oferta 
+    academica */
+    const celdaComparacion = document.getElementById("celdaComparacion" + idCicloMateria);
     /* para que se pueda convertir la hora, se agrega un dia predeterminado 
     ya que lo unico que nos importa es la diferencia de horas */
     // hora de inicio
@@ -18,7 +22,8 @@ function stringToTime(horaInicio, horaFin, idCicloMateria) {
     const horaClase = (Math.abs(date2.valueOf() - date1.valueOf())) / 3600000;
     // se suman las horas al acumulador
     horasTotalClase += horaClase;
-    // se cambia el valor de la celda a las horas totales
+    // se cambia el valor de las celdas a las horas totales
     celda.innerText = horasTotalClase;
+    celdaComparacion.innerText = horasTotalClase;
     return horaClase;
 }
