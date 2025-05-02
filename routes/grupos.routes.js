cae9a2d1c5d1c9ea857553bec97d4f413f8ad9f2
XimePerezEscalante <A01751827@tec.mx>
2025-05-02 12:52:19 -0600

    [UPDATE] ruta para asignar salones a grupos existentes

--- routes/grupos.routes.js
+++ routes/grupos.routes.js
@@ -6,7 +6,9 @@ const horariosController = require('../controllers/horarios.controller');
 const isAuth = require('../util/is-auth');
 const canView = require('../util/canViewAllGroups');
 
-router.post('/asignar-salon', gruposController.asignarSalonPorMateria);
+router.get('/asignar/existente/:idGrupo/:idSalon', isAuth, canView, gruposController.get_asignar_salon_grupo);
+
+router.post('/asignar-salon', isAuth, gruposController.asignarSalonPorMateria);
 
 // nueva ruta para fetch("/grupos/datos")
 router.get('/datos', isAuth, gruposController.getDatosGrupos);
