326c40bd81e27fa8afa19973b055feb155681d21
XimePerezEscalante <A01751827@tec.mx>
2025-05-05 16:09:37 -0600

    [UPDATE] agregra rutas para editar el horario del alumno y eliminar grupos del mismo

--- routes/alumnos.routes.js
+++ routes/alumnos.routes.js
@@ -7,6 +7,8 @@ const canViewSelf = require('../util/canViewAssignedGroups');
 
 const alumnosController = require('../controllers/alumnos.controller');
 
+router.get('/:idCiclo/horario/:idIVD/eliminar/:idMateria', isAuth, canViewAll, alumnosController.get_eliminar_grupo);
+router.get('/:idCiclo/horario/:idIVD/editar', isAuth, canViewAll, alumnosController.get_editar_horario);
 router.get('/:idCiclo/horario/:idIVD', isAuth, canViewAll, alumnosController.get_horario);
 router.get('/regulares/:idCiclo', isAuth, canViewSelf, alumnosController.get_horario_alumnos_regulares);
 router.get('/irregulares/:idCiclo', isAuth, canViewSelf, alumnosController.get_horario_alumnos_regulares);
